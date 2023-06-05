a568200f98306b849d9699b43b2453b1c085e852
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2023-06-05 16:00:52 +0530

    done with contactController.js basic before model have to setup

--- Controllers/contactControllers.js
+++ Controllers/contactControllers.js
@@ -1,7 +1,30 @@
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access private
+const getContacts = (req , res) =>{
+
+}
+
+const createContact = (req , res) =>{
+
+}
 
 const getContact = (req , res) =>{
-    
+
 }
+
+const updateContact = (req , res) =>{
+
+}
+
+const deleteContact = (req , res) =>{
+
+}
+
+module.exports = {
+    getContacts , 
+    createContact ,
+    getContact,
+    updateContact,
+    deleteContact
+};
