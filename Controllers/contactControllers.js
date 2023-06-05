5591b48bdeef5abcfadd220fd753da950dd6a6c4
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2023-06-05 16:10:19 +0530

    Updating the routes of the contact API

--- Controllers/contactControllers.js
+++ Controllers/contactControllers.js
@@ -2,23 +2,23 @@
 //@route GET /api/contacts
 //@access private
 const getContacts = (req , res) =>{
-
+    res.json({message : "The server is perfectly working.. "});
 }
 
 const createContact = (req , res) =>{
-
+    res.json({message : "The server is perfectly working.. "});
 }
 
 const getContact = (req , res) =>{
-
+        res.json({message : "The server is perfectly working.. "});
 }
 
 const updateContact = (req , res) =>{
-
+         res.json({message : "The server is perfectly working.. "});
 }
 
 const deleteContact = (req , res) =>{
-
+        res.json({message : "The server is perfectly working.. "});
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/dbConnection');
-
 const dotenv = require('dotenv').config();
 
 const app = express();
@@ -11,8 +10,8 @@ connectDB();
 app.use(express.json());
 
 
-app.use("/api/contacts" , require('./Routes/contactRoutes'));
+app.use("/api/contact" , require('./Routes/contactRoutes'));
 
 app.listen(port , () =>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${port}`);
 })
