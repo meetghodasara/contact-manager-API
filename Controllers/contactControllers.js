13c248d63b9865748c83c16c7327b580a05a0abd
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2023-06-05 17:15:51 +0530

    Adding a model

--- Controllers/contactControllers.js
+++ Controllers/contactControllers.js
@@ -1,30 +1,31 @@
-//@desc Get all contacts
-//@route GET /api/contacts
-//@access private
-const getContacts = (req , res) =>{
-    res.json({message : "The server is perfectly working.. "});
-}
 
-const createContact = (req , res) =>{
-    res.json({message : "The server is perfectly working.. "});
-}
 
-const getContact = (req , res) =>{
-        res.json({message : "The server is perfectly working.. "});
-}
 
-const updateContact = (req , res) =>{
-         res.json({message : "The server is perfectly working.. "});
-}
+const getContacts = (req, res) => {
+  res.json({ message: "The server is perfectly working.. " });
+};
 
-const deleteContact = (req , res) =>{
-        res.json({message : "The server is perfectly working.. "});
-}
+const createContact = (req, res) => {
+  res.json({ message: "The server is perfectly working.. " });
+};
+
+
+const getContact = (req, res) => {
+  res.json({ message: "The server is perfectly working.. " });
+};
+
+const updateContact = (req, res) => {
+  res.json({ message: "The server is perfectly working.. " });
+};
+
+const deleteContact = (req, res) => {
+  res.json({ message: "The server is perfectly working.. " });
+};
 
 module.exports = {
-    getContacts , 
-    createContact ,
-    getContact,
-    updateContact,
-    deleteContact
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
 };
